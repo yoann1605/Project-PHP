114aedad6b8e10e4cd282241b0dd3b451d0b0b63
yoann1605 <you@example.com>
2020-01-18 14:51:04 +0100

    first project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Mon site php</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+        
+    </div>
+  </body>
+</html>
