864813216dafd6848e3748b59bec54662bf73ac6
mlima95 <milan.lima@hotmail.fr>
2020-01-18 16:17:48 +0100

    add header

--- index.php
+++ index.php
@@ -1,34 +1,13 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/index.css">
-    <title>Mon site php</title>
-  </head>
+<?php include("./php/vue/header.php"); ?>
   <body>
     <div class="container-fluid">
-      <?php include("entete.php"); ?>
-
-      <?php include("menus.php"); ?>
-
-
       <div id="corps">
           <h1>Mon super site</h1>
-
           <p>
-              Bienvenue sur mon site !<br />
-
+              Bienvenue sur mon site !
           </p>
       </div>
-
       <!-- Le pied de page -->
-
-      <?php include("pied_de_page.php"); ?>
-
       </body>
-  </html>
-    <?php include("menus.php"); ?>
     </div>
-  </body>
 </html>
---
+++ php/vue/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Mon site php</title>
+  </head>
+<header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="./assets/img/logo.jpg" style="height:70px; width:70px;" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Service client</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</header>
